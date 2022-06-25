d8bed0c637a161a51e458f91e90ce63062eb4c5b
SuryaGitDev <surya.muthiah@gmail.com>
2022-06-25 10:29:11 +0530

    Change Orders dir to ordertemplates dir

--- orders/views.py
+++ orders/views.py
@@ -19,10 +19,10 @@ def order_create(request):
             # clear the cart
             cart.clear()
             return render(request,
-                          'orders/order/created.html',
+                          'orders/ordertemplates/created.html',
                           {'order': order})
     else:
         form = OrderCreateForm()
     return render(request,
-                  'orders/order/create.html',
+                  'orders/ordertemplates/create.html',
                   {'cart': cart, 'form': form})
